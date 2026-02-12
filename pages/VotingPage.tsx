8b726a69587befa5dd9dba768d0d2c09056f5059
Mubessirul Ummah <210411100140@student.trunojoyo.ac.id>
2026-02-12 14:41:05 +0700

    fix button

--- pages/VotingPage.tsx
+++ pages/VotingPage.tsx
@@ -181,21 +181,19 @@ export const VotingPage: React.FC<VotingPageProps> = ({
         })}
       </div>
 
-      {/* FOOTER */}
-      <section className="w-full bg-yellow-400 py-6">
-        <div className="max-w-7xl mx-auto px-6">
-          <div className="flex flex-wrap justify-center items-center gap-12">
-
-            <img
-  src="/images/LOGO SPONSORED BY.png"
-  alt="Chindo Today"
-  className="max-h-24 md:max-h-32 w-auto object-contain"
-/>
-
-
-          </div>
-        </div>
-      </section>
+<footer className="w-full bg-yellow-400 py-6 mt-auto">
+  <div className="max-w-7xl mx-auto px-6">
+    <div className="flex justify-center items-center">
+      <img
+        src="/images/LOGO SPONSORED BY.png"
+        alt="Chindo Today"
+        className="max-h-24 md:max-h-32 w-auto object-contain"
+      />
     </div>
+  </div>
+</footer>
+
+    </div>
+    
   );
 };
